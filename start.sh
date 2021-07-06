390380f1768f330d41395ce4da0ad0c5c2c0160e
myejin <rlagpwlsq78@gmail.com>
2021-07-06 15:43:58 +0000

    설치모듈 변경
    7-8번줄 Node.js로 변경

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ sudo apt-get install -y python3 python3-pip
 sudo pip3 install flask Flask-restful Flask-cors
 export FLASK_APP=app.py
 flask run &
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt update && sudo apt-get install -y nodejs
 cd frontend
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-sudo echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 npm install 
 npm start
